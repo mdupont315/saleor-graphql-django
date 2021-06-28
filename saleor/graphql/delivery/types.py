83d1b6cb4224eb5b2a1da5e2d8107c8d5dbbe8b1
toan.huynhtrankhanh <toan.huynhtrankhanh.ncc@gmail.com>
2021-06-28 17:10:49 +0700

    delivery feature

--- saleor/graphql/delivery/types.py
+++ saleor/graphql/delivery/types.py
@@ -1,7 +1,20 @@
+import graphene 
 from graphene_django import DjangoObjectType
 from ...delivery import models
+from ..core.connection import CountableDjangoObjectType
 
-class Delivery(DjangoObjectType):
+
+class Delivery(CountableDjangoObjectType):
+    delivery_area = graphene.String()
+    delivery_fee = graphene.Float()
+    from_delivery = graphene.Float()
+    min_order = graphene.Float()
     class Meta:
         model = models.Delivery
-        field = ("id",  "delivery_area", "delivery_fee", "from_delivery", "min_order")
+        field = [
+            "id",
+            "delivery_area",
+            "delivery_fee",
+            "from_delivery",
+            "min_order"
+        ]
