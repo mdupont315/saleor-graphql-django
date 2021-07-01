86caf78b070e02f6f80d6ebc2c5b9214cc592b5d
khanhtoandng <khanhtoan.dng@gmail.com>
2021-07-01 15:11:10 +0700

    modify delivery model

--- saleor/delivery/models.py
+++ saleor/delivery/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from saleor.store.models import Store
 from ..core.db.fields import SanitizedJSONField
 from ..core.utils.editorjs import clean_editor_js
+from ..core.models import MultitenantModelWithMetadata
 
 # Create your models here.
-class Delivery(models.Model):
+class Delivery(MultitenantModelWithMetadata):
+    store = models.ForeignKey(
+        Store,
+        related_name="deliveries",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    tenant_id='store_id'
     delivery_area = SanitizedJSONField(blank=True, null=True, sanitizer=clean_editor_js)
     delivery_fee = models.FloatField(blank=True, null=True, default=0)
     from_delivery = models.FloatField(blank=True, null=True, default=0)
     min_order = models.FloatField(blank=True, null=True, default=0)
 
+    def __str__(self) ->str:
+        return self.name
     
