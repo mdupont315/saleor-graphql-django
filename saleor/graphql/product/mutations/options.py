b7bab6ce1a4b9a00925431984390e3f3a004fb01
tuan.vuthanh <tuan.vuthanh@ncc.asia>
2021-07-01 08:32:17 +0700

    add product option for product

--- saleor/graphql/product/mutations/options.py
+++
@@ -1,8 +0,0 @@
-# import graphene
-# from ...core.mutations import BaseMutation
-
-# class OptionInput(graphene.InputObjectType):
-#     name = graphene.String(required=True, description="Name")
-#     description = graphene.String(required=True, description="Description")
-
-# class OptionCreate
