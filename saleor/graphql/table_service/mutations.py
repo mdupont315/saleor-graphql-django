f23e69fd7de10c0182313b2b25b4e160d9c0cc87
tuan.vuthanh <tuan.vuthanh@ncc.asia>
2021-08-10 13:25:30 +0700

    fix table service API

--- saleor/graphql/table_service/mutations.py
+++ saleor/graphql/table_service/mutations.py
@@ -5,6 +5,7 @@ import graphene
 from ..core.mutations import ModelBulkDeleteMutation, ModelDeleteMutation, ModelMutation
 from ...table_service import models
 from ...core.permissions import TableServicePermissions
+from graphql_relay.node.node import from_global_id
 
 class TableServiceInput(graphene.InputObjectType):
     table_name = graphene.String(
@@ -49,14 +50,43 @@ class TableServiceCreate(ModelMutation):
         error_type_class = TableServiceError
         error_type_field = "table_service_errors"
 
+class TableServiceUpdateInput(graphene.InputObjectType):
+    table_name = graphene.String(
+        description="table name",
+    )
+    table_qr_code = graphene.String(
+        description="table qr code",
+    )
+    active = graphene.Boolean(
+        description="status of qr code",
+    )
 
-class TableServiceUpdate(TableServiceCreate):
+class TableServiceUpdate(ModelMutation):
     class Arguments:
         id = graphene.ID(required=True, description="ID of a table service to update.")
-        input = TableServiceInput(
+        input = TableServiceUpdateInput(
             required=True, description="Fields required to table service time."
         )
 
+    @classmethod
+    def perform_mutation(cls, _root, info, **data):
+        # validate table name
+        input = data.get("input")
+        table_name = input.get("table_name", None)
+        if table_name:
+            _type , current_table_service_pk = from_global_id(data["id"])
+            table_service = models.TableService.objects.filter(table_name=table_name).first()
+            if table_service and table_service.id != int(current_table_service_pk):
+                raise ValidationError(
+                    {
+                        "table_name": ValidationError(
+                            "table name already exists.",
+                            code=TableServiceErrorCode.ALREADY_EXISTS,
+                        )
+                    }
+                )
+        return super().perform_mutation(_root, info, **data)
+    
     class Meta:
         description = "Update service time."
         model = models.TableService
