92a9787c83a03d6e08be174b4985cf5fe4abd23e
tuan.vuthanh <tuan.vuthanh@ncc.asia>
2021-08-19 11:27:26 +0700

    fix logging

--- saleor/core/utils/logging.py
+++ saleor/core/utils/logging.py
@@ -7,7 +7,8 @@ logger = logging.getLogger('django')
 def log_info(component, message=None, content=None):
     logger.info('--{component}--'.format(component=component))
     store = get_current_tenant()
-    logger.info('--Strore_Id: {store}--'.format(store=store.id))
+    if store:
+        logger.info('--Strore_Id: {store}--'.format(store=store.id))
     if message:
         logger.info(message)
     if content:
