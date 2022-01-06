70329f88db62aaf7fbb99e959ee239b3a93b767a
thang <kuthang32@gmail.com>
2022-01-06 14:04:57 +0700

    add commit

--- saleor/plugins/email_common.py
+++ saleor/plugins/email_common.py
@@ -186,6 +186,7 @@ def list_product_customer(this, options, items, channel, channel_symbol):
     TWOPLACES = Decimal(10) ** -2       # same as Decimal('0.01')
     result = [u'<table class="product-table">']
     for thing in items:
+        # logging.getLogger('django').info('---line----{line}------'.format(line=thing.__dict__) )
         result.append(u'<tr>')
         result.append(u'<td class="td-number">')
         result.append(str(thing.quantity))
@@ -224,22 +225,75 @@ def list_product_customer(this, options, items, channel, channel_symbol):
     result.append(u'</table>')
     return result
 
+def list_product_customer_admin(this, options, items, channel, channel_symbol):
+    TWOPLACES = Decimal(10) ** -2       # same as Decimal('0.01')
+    result = [u'<div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:12px;line-height:14px;text-align:left;color:#000;">hahaha</div>']
+    logging.getLogger('django').info('---line----{line}------'.format(line="heloooooooo") )
+
+    # for thing in items:
+    #     logging.getLogger('django').info('---line----{line}------'.format(line=thing.__dict__) )
+
+#         result.append(u'<tr>')
+#         result.append(u'<td align="left" style="font-size: 0px; padding: 8px; word-break: break-word; width: 50%;">')
+# # product info
+#         result.append(u'<div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:12px;text-align:left;color:#000; display: flex">')
+#         result.append(u'<p style="margin: 0; margin-right: 8px; font-size: 12px;line-height: 14px;font-weight: bold;">')
+#         result.append('{quantity}x'.format(quantity = str(thing.quantity)))
+#         result.append(u'</p>')
+#         result.append(u'<div>')
+#         result.append(u'<p style="margin: 0; font-size: 12px;line-height: 14px;font-weight: bold;">')
+#         result.append(thing.product_name)
+#         result.append(u'</p>')
+# #  option 
+#         option_values = thing.option_values.all()
+#         if option_values:
+#             logging.getLogger('django').info('---optionm----{sender_name}------'.format(sender_name="asdasdasd") )
+
+#             for option_value in option_values:
+#                 result.append(u'<p style="margin: 0; font-size: 12px;line-height: 14px;font-weight: 400;">')
+#                 result.append("{option} : {name} ({curency} {price})".format(
+#                     option=option_value.option.name,
+#                     name=option_value.name,
+#                     curency=channel_symbol,
+#                     price=formatComma((quantize_price(option_value.get_price_amount_by_channel(
+#                         channel), channel)).quantize(TWOPLACES))
+#                 ))
+#                 result.append(u'</p>')
+#         result.append(u'</div>')
+# # price
+#         result.append(u'<p style="margin: 0; font-size: 12px;line-height: 14px;font-weight: bold; text-align: right;flex: 1;">')
+#         # result.append(str((quantize_price(thing.total_price_net.amount, channel)).quantize(TWOPLACES)))
+#         result.append("{curency} {price}".format(
+#             curency=channel_symbol,
+#             price=  formatComma((quantize_price(thing.total_price_net.amount, channel)).quantize(TWOPLACES)) 
+#             # quantize_price(thing.total_price_net.amount, channel)
+           
+#         ))
+
+#         logging.getLogger('django').info('---price----{sender_name}------'.format(sender_name=thing.total_price_net.amount) )
+
+#         result.append(u'</p></div></td></tr>')
+        
+
+    return result
 
 def customer_list_address(this, options, items):
-    result = [u'<ul>']
+    result = [u'']
     dict_items = items.items()
     ctm_name = items.get('first_name', "") + " " + items.get('last_name', "")
-    result.append(u'<li>')
+    logging.getLogger('django').info('---ctm_name----{ctm_name}------'.format(ctm_name=ctm_name) )
+    print(ctm_name,"=========================ctm_name")
+    result.append(u'<div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:12px;line-height:14px;text-align:left;color:#000;">')
     result.append(str(ctm_name)),
-    result.append(u'</li>')
+    result.append(u'</div>')
     for key, value in dict_items:
         if key == 'id' or key == '_state' or key == 'last_name' or key == 'first_name':
             continue
         if value:
-            result.append(u'<li>')
+            result.append(u'<div style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:12px;line-height:14px;text-align:left;color:#000;">')
             result.append(str(value)),
-            result.append(u'</li>')
-    result.append(u'</ul>')
+            result.append(u'</div>')
+    # result.append(u'</ul>')
     return result
 
 
@@ -359,6 +413,7 @@ def send_email(config: EmailConfig, recipient_list, context, subject="", templat
         "get_product_image_thumbnail": get_product_image_thumbnail,
         "compare": compare,
         "product_customer": list_product_customer,
+        "product_customer_admin": list_product_customer_admin,
         "address_customer": customer_list_address,
         "address_customer_delivery": customer_list_address_delivery,
         "address_customer_pickup": customer_list_address_pickup,
